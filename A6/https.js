12cb6e60427429399456d360dbaa26f5c69309a1
unknown <yutongsu@asu.edu>
2021-03-25 02:16:02 +0800

    111

--- A6/https.js
+++ A6/https.js
@@ -2,9 +2,9 @@ var homeDir = require('path').join(require('os').homedir());
 require('greenlock-express').create({
   version: 'draft-11'
 , server: 'https://acme-v02.api.letsencrypt.org/directory'
-, email: ''                                     // CHANGE THIS
+, email: 'yutongsu@asu.edu'                                     // CHANGE THIS
 , agreeTos: true
-, approveDomains: [ 'example.com', 'www.example.com' ]              // CHANGE THIS
+, approveDomains: [ 'yutongsu.httpsexample.xyz', 'www.yutongsu.httpsexample.xyz' ]              // CHANGE THIS
 , store: require('greenlock-store-fs')
 , configDir: homeDir
 , app: require('./server.js')
